b1aec64b21259b32797367bddd443be20ae177fa
Liam <ljackson61@my.bcit.ca>
2024-10-06 21:46:00 -0700

    Initial Google SSO setup

--- Earls_Discount_System/Earls_Discount_System/urls.py
+++ Earls_Discount_System/Earls_Discount_System/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('login/', include('login.urls')),  # For login routes
     path('employee/', include('employee.urls')),  # For employee-related routes
     path('admin_panel/', include('site_admin.urls')),  # For admin panel routes
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.socialaccount.urls'))
 ]
