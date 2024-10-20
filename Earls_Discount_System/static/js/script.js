e74f4ad79b0dbca77bbeaec52d7e33ed55c7e067
swimmingwebdev <swimmingwebdev@gmail.com>
2024-10-19 19:34:16 -0700

    style/update for responsive design

--- Earls_Discount_System/static/js/script.js
+++ Earls_Discount_System/static/js/script.js
@@ -1,3 +1,4 @@
+// Copy email to clipboard
 function copyEmail() {
   const email = document.getElementById('email').innerText;
   navigator.clipboard
@@ -8,4 +9,15 @@ function copyEmail() {
     .catch((err) => {
       console.error('Failed to copy: ', err);
     });
+}
+
+// NavBar for mobile view
+function myMenu() {
+  var navLinks = document.getElementById('nav-items');
+  var dropdown1 = document.getElementById('dropdown-content1');
+  var dropdown2 = document.getElementById('dropdown-content2');
+
+  navLinks.classList.toggle('active');
+  dropdown1.classList.toggle('active');
+  dropdown2.classList.toggle('active');
 }
