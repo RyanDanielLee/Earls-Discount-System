7ab63fe5e0c8dd10161982d30c2ea12766c9a436
swimmingwebdev <swimmingwebdev@gmail.com>
2024-11-19 13:39:40 -0800

    Update SendGrid library

--- Earls_Discount_System/Earls_Discount_System/settings.py
+++ Earls_Discount_System/Earls_Discount_System/settings.py
@@ -93,12 +93,12 @@ DATABASES = {
 }
 
 # SendGrid SMTP Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD =  os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD =  os.getenv('EMAIL_HOST_PASSWORD')
 
 
 # Password validation
